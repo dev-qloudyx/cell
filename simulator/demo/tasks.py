b82680e90493085901b413caa6018063ffd38c1c
Victor Costa <victor.costa@qloudyx.pt>
2023-10-03 15:00:04 +0100

    UPDATE task & Dockerfile

--- simulator/demo/tasks.py
+++ simulator/demo/tasks.py
@@ -3,8 +3,10 @@ from django.core.mail import EmailMessage
 from celery import shared_task
 
 @shared_task
-def send_mail_after_delay(email_subject, email_body, from_email, recipient_list):
+def send_mail_after_delay(email_subject, email_body, from_email, recipient_list, video_file, report_file):
     mail = EmailMessage(email_subject, email_body, from_email, recipient_list)
+    mail.attach_file(video_file)
+    mail.attach_file(report_file)
     mail.send()
 
     return f"Task created"
