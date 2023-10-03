5f9ba1e264041cf2b877bc82a3fe59f7af365b49
muffs98 <mafalda.lopes@qloudyx.pt>
2023-10-03 14:45:30 +0100

    Atualizei o email que vai para o host

--- simulator/demo/views.py
+++ simulator/demo/views.py
@@ -87,7 +87,7 @@ class ClientForm(CreateView):
     #Mail to self(host) - Este email é para o host com os dados do cliente
 
         # email_subject2 = 'Simulation submitted from ' + str(name) + ' from company ' + str(company)
-        # email_body2 = 'New simulation submitted.'
+        # email_body2 = 'New simulation submitted from ' + str(email1) +  ' .'
         # from_email2 = settings.EMAIL_HOST_USER
         # recipient_list2 = settings.EMAIL_HOST_USER
         # mail3 = EmailMessage(email_subject2, email_body2, from_email2, recipient_list2)
